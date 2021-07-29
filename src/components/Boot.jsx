4aab2605535fa9f5918bc73656a6f1b21dba7474
srilekha <jsrilekha.17@gmail.com>
2021-07-30 01:22:26 +0530

    first commit

---
+++ src/components/Boot.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Boot() {
+    return (
+        <div className="note">
+            <h1 >
+            The bibliophile's heaven
+            </h1>
+            <p >
+                Books are important because they  are the key to an open and intelligent society. They provide a safe place for any intellectual to store his or her thoughts and transports these thoughts from person to person.
+            </p>
+            
+        </div>
+    )
+}
+
+export default Boot
